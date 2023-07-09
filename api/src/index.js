f63c0750bcd3a1d317ad78018adb2e74f303c861
dandeliao <danielramos@tuta.io>
2023-07-08 21:19:48 -0300

    reestrutura tratamento de erros

--- api/src/index.js
+++ api/src/index.js
@@ -8,8 +8,8 @@ const PostgreSqlStore = require('connect-pg-simple')(session); // para armazenam
 const pool = require('./config/database'); // para armazenamento de sesssão
 require('./config/passport');
 const cors = require('cors');
-const errorHandler = require('./middlewares/errorHandler');
 const passport = require('passport');
+const customError = require('http-errors');
 require('dotenv').config();
 const PORT = process.env.port || 4000;
 
@@ -32,7 +32,6 @@ app.use(cors({
 app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(errorHandler);
 
 // rotas
 app.use('/bichos', 		rotasBichos		);
@@ -54,6 +53,17 @@ app.get('/', async (req, res, next) => {
 	} catch (erro) {
 		next(erro);
 	}
+});
+
+// tratamento de erros
+app.use((req, res, next) => {
+	next(customError(404, 'Página não encontrada.'));
+});
+app.use((error, req, res, next) => {
+	res.status(error.status || 500).json({
+		status: error.status,
+		message: error.message
+	});
 });
 
 // roda o servidor
